b945043da7a474ad01c562c9988fbcfa230f49ae
Samuel Keller <kellesam@flip2.engr.oregonstate.edu>
2017-04-26 16:35:10 -0700

    Added algorithm 3

---
+++ mcc_algos.py
@@ -0,0 +1,35 @@
+# MCC algorithm definitions
+
+
+# Algorithm 3: Dynamic Programming
+def dp(coins, value):
+	results = []
+	for i in range(len(coins)):
+		results.append(0)
+	
+	coinTracker = []
+	for i in range(value + 1):
+		coinTracker.append(-1)
+	
+	knownCoins = []
+	knownCoins.append(0)
+	for i in range(1, value + 1):
+		knownCoins.append(value + 1)
+	
+	for i in range(1, value + 1):
+		minCoins = value + 1
+	
+		for j in range(0, len(coins)):
+			if i >= coins[j]:
+				minCoins = min(minCoins, knownCoins[i - coins[j]])
+				
+			if minCoins + 1 < knownCoins[i]:
+				knownCoins[i] = minCoins + 1
+				coinTracker[i] = j
+	
+	curIndex = len(coinTracker) - 1
+	while curIndex > 0:
+		results[coinTracker[curIndex]] += 1
+		curIndex -= coins[coinTracker[curIndex]]
+	
+	return results
