e75a0de0c76cf0917142a9a134aa237c5f59120d
August Lautt <lautta@oregonstate.edu>
2017-05-02 13:04:04 -0400

    Added command line algorithm selection

--- mcc.py
+++ mcc.py
@@ -29,23 +29,37 @@ def write_results(filename, algo_func, algo_name, values, amounts, write_option)
 			change_result = ' '.join([str(i) for i in change_result])
 			output.write('\n%s\n%s\n' % (change_result, count_result))
 		output.write('\n')
+	print('Wrote %s to %s file...' % (algo_name, filename))
 
 
 if __name__ == '__main__':
 
-	if len(sys.argv) > 2:
-		print('Usage: python mcc { filename.txt }')
+	if len(sys.argv) > 3 or len(sys.argv) == 2:
+		print('Usage: python mcc.py { changeslow || changegreedy || changedp } { filename.txt }')
 		exit(1)
-	elif len(sys.argv) == 2:
-		input_file = str(sys.argv[1])
+	elif len(sys.argv) == 3:
+		input_file = str(sys.argv[2])
+		output_file = input_file.split('.')[0] + 'change.txt'
+		algorithm = str(sys.argv[1])
+		v, a = read_problems(input_file)
+
+		if algorithm == 'changeslow':
+			write_results(output_file, changeslow, 'changeslow', v, a, 'ab+')
+		elif algorithm == 'changegreedy':
+			write_results(output_file, changegreedy, 'changegreedy', v, a, 'ab+')
+		elif algorithm == 'changedp':
+			write_results(output_file, changedp, 'changedp', v, a, 'ab+')
+		elif algorithm == 'all':
+			write_results(output_file, changeslow, 'changeslow', v, a, 'wb')
+			write_results(output_file, changegreedy, 'changegreedy', v, a, 'ab')
+			write_results(output_file, changedp, 'changedp', v, a, 'ab')
+		else:
+			print('%s not recognized.\nUsage: python mcc.py { changeslow || changegreedy || changedp } { filename.txt }' % algorithm)
 	else:
 		input_file = 'test.txt'
-
-	output_file = input_file.split('.')[0] + 'change.txt'
-
-	v, a = read_problems(input_file)
-	write_results(output_file, changeslow, 'changeslow', v, a, 'wb')
-	write_results(output_file, changegreedy, 'changegreedy', v, a, 'ab')
-	write_results(output_file, changedp, 'changedp', v, a, 'ab')
-
-	print('Wrote %s file...' % output_file)
+		output_file = input_file.split('.')[0] + 'change.txt'
+		v, a = read_problems(input_file)
+		write_results(output_file, changeslow, 'changeslow', v, a, 'wb')
+		write_results(output_file, changegreedy, 'changegreedy', v, a, 'ab')
+		write_results(output_file, changedp, 'changedp', v, a, 'ab')
+		print('Wrote all three algorithms to %s' % output_file)
